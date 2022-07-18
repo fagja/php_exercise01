fe32bc78c4cfbecf9e2fd98296616c643fc477c1
fagja <hey.shunya19@gmail.com>
2022-07-18 23:57:38 +0900

    format_files

--- 03/03_array.php
+++ 03/03_array.php
@@ -7,13 +7,13 @@ $nums = [
 ];
 
 // ここにコードを記述
-echo $nums[1][1].PHP_EOL;
-echo $nums[2][2].PHP_EOL;
-echo $nums[2][1].PHP_EOL;
-echo $nums[2][0].PHP_EOL;
-echo $nums[0][0].PHP_EOL;
-echo $nums[1][0].PHP_EOL;
-echo $nums[0][1].PHP_EOL;
+echo $nums[1][1] . PHP_EOL;
+echo $nums[2][2] . PHP_EOL;
+echo $nums[2][1] . PHP_EOL;
+echo $nums[2][0] . PHP_EOL;
+echo $nums[0][0] . PHP_EOL;
+echo $nums[1][0] . PHP_EOL;
+echo $nums[0][1] . PHP_EOL;
 
 // 4
 // 7
