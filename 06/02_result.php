8a0d4ea6014de849c3e6b5372ce5ddae2b571299
fagja <hey.shunya19@gmail.com>
2022-07-21 12:26:50 +0900

    phpをhtmlへ

--- 06/02_result.php
+++ 06/02_result.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo htmlspecialchars("私の名前は、{$_POST['name']}です。", ENT_QUOTES, 'UTF-8');
+    $msg = "私の名前は、{$_POST['name']}です。";
 }
 ?>
 
@@ -16,9 +16,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 
 <body>
+    <div>
+        <?= htmlspecialchars($msg, ENT_QUOTES, 'UTF-8') ?>
+    </div>
     <p></p>
-    <p></p>
-    <a href="02_form.php">戻る</a>
+    <div>
+        <a href="02_form.php">戻る</a>
+    </div>
 </body>
 
 </html>
